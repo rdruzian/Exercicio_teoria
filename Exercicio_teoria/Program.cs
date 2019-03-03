7b191f2c687a013b5b36b98c3cc68b2525fbea23
rdruzian <rendruzian@gmail.com>
2019-03-02 22:23:31 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_teoria/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_teoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alfabeto, variaveis;
+            Dictionary<int, string> digitados = null;
+            List<int> ordem = null;
+            Tuple<int,string,string> regra;
+            char inicio;
+            int count = 0;
+
+            Console.WriteLine("Entre com as variaveis (separadas por ,): ");
+            string aux = Console.ReadLine();
+            variaveis = aux.Replace(",", " ").ToCharArray();
+
+            Console.WriteLine("Entre com o alfabeto (separadas por ,): ");
+            aux = Console.ReadLine();
+            alfabeto = aux.Replace(",", " ").ToCharArray();
+
+            aux = null;
+
+            while (aux != "0")
+            {
+                Console.WriteLine("Regras: ");
+                aux = Console.ReadLine();
+                digitados.Add(count++, aux);
+            }
+            Console.WriteLine("Ordem das regras: ");
+            foreach(KeyValuePair<int, string> valor in digitados)
+            {
+                Console.WriteLine(valor);
+            }
+            while(aux != "@")
+            {
+                Console.WriteLine("Digite a ordem (@ para terminar): ");
+                ordem.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+        }
+    }
+}
